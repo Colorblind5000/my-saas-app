eaf03c3bc66bd1e086d5067478c6106df8e83729
Colorblind5000 <127291701+Colorblind5000@users.noreply.github.com>
2024-10-30 15:34:46 +0100

    Opdateret mappestruktur og stier

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 // next.config.js
-const path = require('path');
-
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['tsx', 'ts', 'js', 'jsx'],
-  experimental: {
-    appDir: false, // Hvis du ikke bruger en app-mappe
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
+  
