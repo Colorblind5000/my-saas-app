0d756fc52e05e01d613ff6d078ce41950338334d
Colorblind5000 <emil@pixed.com>
2024-10-30 20:26:56 +0100

    Omdøbt Pages til pages

---
+++ pages/Work_in_progress_01.tsx
@@ -0,0 +1,14 @@
+// src/pages/Work_in_progress_01.tsx
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+
+const WorkInProgress01: React.FC = () => {
+  return (
+    <Box sx={{ padding: 3 }}>
+      <Typography variant="h4">Work in Progress 01</Typography>
+      <Typography variant="body1">Denne side er under udvikling.</Typography>
+    </Box>
+  );
+};
+
+export default WorkInProgress01;
